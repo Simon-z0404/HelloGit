8c12299cfdc56b89cda2823f75d8d922817819a0
Simon <1299327589@qq.com>
2022-12-27 21:15:19 +0800

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sort(vector<int>& vec) {
+
+}
+
+int main() {
+	vector<int> vec = {10, 12, 1, 4, 3};
+	sort(vec);
+	return 0;
+}
